ae181e0a1bc490e0b721cfccd4baae42c872da09
nirvana47 <nirvana47@gmail.com>
2023-01-09 15:59:29 -0800

    Removed switch code and added explanation

--- 20. Valid Parentheses/src/Solution.java
+++ 20. Valid Parentheses/src/Solution.java
@@ -5,63 +5,34 @@ public class Solution {
         if (s.length() == 1) return false;
 
         Stack<Character> parenthesesStack = new Stack<>();
-        System.out.println("Current Stack looks like this: " + parenthesesStack.toString());
-        // parenthesesStack.pop();
 
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
                 parenthesesStack.push(s.charAt(i));
-                System.out.println("Current Stack looks like this: " + parenthesesStack.toString());
+                // System.out.println("Current Stack looks like this: " + parenthesesStack.toString());
                 // System.out.println();
             } else if (parenthesesStack.isEmpty()) {
                 return false;
             }
             else if (s.charAt(i) == ')' && parenthesesStack.peek() == '(') {
-                // parenthesesStack.pop();
-                System.out.println(parenthesesStack.pop());
+                parenthesesStack.pop();
+                // System.out.println(parenthesesStack.pop());
             } 
             else if (s.charAt(i) == ']' && parenthesesStack.peek() == '[') {
-                // parenthesesStack.pop();
-                System.out.println(parenthesesStack.pop());
+                parenthesesStack.pop();
+                // System.out.println(parenthesesStack.pop());
             }
             else if  (s.charAt(i) == '}' && parenthesesStack.peek() == '{') {
-                // parenthesesStack.pop();
-                System.out.println(parenthesesStack.pop());
+                parenthesesStack.pop();
+                // System.out.println(parenthesesStack.pop());
             }
             else 
                 return false;
-            /* switch (s.charAt(i)) {
-                case '(':
-                case '[':
-                case '{':
-                    parenthesesStack.push(s.charAt(i));
-                    break;
-                case ')':
-                    if (parenthesesStack.peek() == ')') {
-                        parenthesesStack.pop();
-                        break;
-                    } else {
-                        return false;
-                    }
-                case '}':
-                    if (parenthesesStack.peek() == '{') {
-                        parenthesesStack.pop();
-                        break;
-                    } else {
-                        return false;
-                    }
-                case ']':
-                    if (parenthesesStack.peek() == '[') {
-                        parenthesesStack.pop();
-                        break;
-                    } else {
-                        return false;
-                    }
-                default:
-                    return false;
-            }*/
         }
 
+        // AFTER ALL THE POPPING AND PUSHING, IF THE STACK HAS STUFF REMAINING, THAT MEANS
+        // BRACKETS HAVE NOT BEEN CLOSED; WHICH MEANS IT IS FALSE. BUT IF IT IS EMPTY, THEN
+        // IT IS GOOD TO GO!
         if (!parenthesesStack.empty()) {
             return false;
         } else {
