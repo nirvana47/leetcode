575ce9ca189028b7ed2b428afc8296ecf7d849e6
nirvana47 <nirvana47@gmail.com>
2023-01-12 13:31:09 -0800

    Using Floyd's Tortoise and Hare Algorithm
    In the while loop, if you do the condition check like so: `oneStepPointer.next != null && twoStepPointer.next.next != null` for some reason getting a runtime execution; need to understand why.

---
+++ 142. Linked List Cycle ||/Solution.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+     
+        // if(head == null || head.next == null) return false;
+        // ListNode fast = head, slow = head;        
+        // while(fast.next != null && fast.next.next != null){
+        //     fast = fast.next.next;
+        //     slow = slow.next;
+        //     if(fast == slow) return true;
+        // }
+        // return false;
+
+        if (head == null || head.next == null)
+            return false;
+
+        ListNode twoStepPointer = head;
+        ListNode oneStepPointer = head;
+
+        while (twoStepPointer.next != null && twoStepPointer.next.next != null) {
+            oneStepPointer = oneStepPointer.next;
+            twoStepPointer = twoStepPointer.next.next; 
+            if (oneStepPointer == twoStepPointer)
+                return true;
+        }
+
+        return false;
+
+    }
+}
