efeba176e4cce3413e45f02eaa2c300ad3856461
nirvana47 <nirvana47@gmail.com>
2024-02-15 21:40:43 -0800

    Add files via upload

--- 128. Longest Consecutive Sequence/src/App.java
+++ 128. Longest Consecutive Sequence/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Solution sol = new Solution();
+        int[] nums = {100, 4, 200, 1, 2, 3};
+
+        System.out.println("Longest Consecutive Sequence: " + sol.longestConsecutive(nums));
     }
 }
