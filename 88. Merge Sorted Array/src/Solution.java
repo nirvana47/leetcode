8e69d6508254ae3ca0d24cc06c534e2645d81176
nirvana47 <nirvana47@gmail.com>
2024-02-17 11:02:52 -0800

    How is this fucking easy?
    The while loop does all the edge cases too... did not need the if...else at the top... so confused. Wow.

--- 88. Merge Sorted Array/src/Solution.java
+++ 88. Merge Sorted Array/src/Solution.java
@@ -46,24 +46,24 @@ public class Solution {
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        if (m >=1 && n == 0)
-            return;
-        else if (m == 0 && n == 1) {
-            nums1[0] = nums2[0];
-            return;
-        } else if (m == 0 && n == 0) {
-            return;
-        } else if (m == 0 && n > 0) {
-            for (int i = m, j = 0; i < m + n && j < n; i++, j++) {
-                nums1[i] = nums2[j];
-            }
-            return;
-        } else if (nums2[0] >= nums1[m - 1]) {
-            for (int i = m, j = 0; i < m + n && j < n; i++, j++) {
-                nums1[i] = nums2[j];
-            }
-            return;
-        } 
+        // if (m >=1 && n == 0)
+        //     return;
+        // else if (m == 0 && n == 1) {
+        //     nums1[0] = nums2[0];
+        //     return;
+        // } else if (m == 0 && n == 0) {
+        //     return;
+        // } else if (m == 0 && n > 0) {
+        //     for (int i = m, j = 0; i < m + n && j < n; i++, j++) {
+        //         nums1[i] = nums2[j];
+        //     }
+        //     return;
+        // } else if (nums2[0] >= nums1[m - 1]) {
+        //     for (int i = m, j = 0; i < m + n && j < n; i++, j++) {
+        //         nums1[i] = nums2[j];
+        //     }
+        //     return;
+        // } 
 
         int i = m - 1;
         int j = n - 1;
