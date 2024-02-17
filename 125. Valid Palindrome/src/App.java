fc9947f85e42d2b414332f87f48dd4e5105d6325
nirvana47 <nirvana47@gmail.com>
2024-02-17 11:09:06 -0800

    Add files via upload

---
+++ 125. Valid Palindrome/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        String s = "A man, a plan, a canal: Panama";
+        Solution sol = new Solution();
+        System.out.println(sol.isPalindrome(s));
+    }
+}
