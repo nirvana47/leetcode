0ae0053a6fed81f3de67c1dd227c5869402791e9
nirvana47 <nirvana47@gmail.com>
2024-02-19 23:47:57 -0800

    Add files via upload

--- 167. Two Sum II - Input Array Is Sorted/src/App.java
+++ 167. Two Sum II - Input Array Is Sorted/src/App.java
@@ -1,11 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
 
         Solution sol = new Solution();
-        int[] nums = { 1, 2, 3, 4 };
-        int target = 3;
+        // int[] nums = { 2, 7, 11, 15 };
+        // int target = 9;
 
-        nums = sol.twoSum(nums, target);
+        // int[] nums = { 2, 3, 4, 15 };
+        // int target = 6;
+        
+        int[] nums = { -1, 0, 2, 3, 4, 15 };
+        int target = -1;
+
+        int[] res = new int[2];
+        res = sol.twoSum(nums, target);
+
+        for(int i : res) {
+            System.out.println("The indices are: " + res[0] + " and: " + res[1]);
+            System.out.println("Thanks!");
+        }
     }
 }
--- 167. Two Sum II - Input Array Is Sorted/src/Solution.java
+++ 167. Two Sum II - Input Array Is Sorted/src/Solution.java
@@ -1,5 +1,17 @@
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        return numbers;
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (start < end) {
+            if (target - numbers[start] == numbers[end]) {
+                return new int[] { start + 1, end + 1 };
+            } else if (target - numbers[start] < numbers[end]) {
+                end--;
+            } else if (target - numbers[start] > numbers[end]) {
+                start++;
+            }
+        }
+        return new int[] { start + 1, end + 1 };
     }
 }
