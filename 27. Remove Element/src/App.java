a27220c6bac55d1b0e67ff785cc98b54a45766b7
nirvana47 <nirvana47@gmail.com>
2024-03-02 01:42:13 -0800

    Add files via upload

--- 27. Remove Element/src/App.java
+++ 27. Remove Element/src/App.java
@@ -3,8 +3,11 @@ public class App {
         System.out.println("Hello, World!");
 
         Solution sol = new Solution();
-        int[] nums = new int[] {0,1,2,2,3,0,4,2};
-        int val = 2;
+        // int[] nums = new int[] {0,1,2,2,3,0,4,2};
+        // int val = 2;
+
+        int[] nums = new int[] { 3, 2, 2, 3 };
+        int val = 3;
 
         System.out.println(sol.removeElement(nums, val));
     }
--- 27. Remove Element/src/Solution.java
+++ 27. Remove Element/src/Solution.java
@@ -1,11 +1,31 @@
 public class Solution {
     public int removeElement(int[] nums, int val) {
-        int i = 0;
-        int k = 0;
+        int frontPtr = 0;
+        int backPtr = nums.length - 1;
+        // int k = 0;
 
-        while (i < nums.length) {
+        while (frontPtr <= backPtr) {
+            if (nums[frontPtr] != val) {
+                frontPtr++;
+                System.out.println("Front Ptr1: " + frontPtr);
+            } else {
+                // k++;
+                // System.out.println("k in first else loop: " + k);
+                if (nums[backPtr] == val) {
+                    backPtr--;
+                    System.out.println("back Ptr1: " + backPtr);
+                    // k++;
+                    // System.out.println("k in 2nd if loop: " + k);
+                } else {
+                    nums[frontPtr] = nums[backPtr];
+                    nums[backPtr] = val;
+                    frontPtr++;
+                    backPtr--;
+                }
+            }
+            System.out.println("Front Ptr2: " + frontPtr);
+            System.out.println("back Ptr2: " + backPtr);
         }
-
-        return k;
+        return frontPtr;
     }
 }
